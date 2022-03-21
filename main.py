0a3801d026f41a410a6bcfb03e859adab80252f6
deep-sarkar <deepsarkar96@gmail.com>
2022-03-21 09:24:22 +0530

    [Raj]Added functionality for stack

--- main.py
+++ main.py
@@ -4,7 +4,29 @@ class Stack:
         self.max_stack = []
 
     def push(self, ele):
-        pass
+        min_ele = ele
+        max_ele = ele
+        if self.min_stack:
+            min_ele = min(min_ele, self.min_stack[-1])
+
+        if self.max_stack:
+            max_ele = max(max_ele, self.max_stack[-1])
+
+        self.max_stack.append(max_ele)
+        self.min_stack.append(min_ele)
 
     def display(self):
-        pass
+        print('min_stack : {}'.format(self.min_stack))
+        print('max_stack : {}'.format(self.max_stack))
+
+
+if __name__ == "__main__":
+    st = Stack()
+    st.push(3)
+    st.push(6)
+    st.push(2)
+    st.push(7)
+    st.push(1)
+    st.push(9)
+    st.push(0)
+    st.display()
