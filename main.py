c21a9c5b8b580ffa797dc4b55b4baa0695da6e73
deep-sarkar <deepsarkar96@gmail.com>
2022-03-21 09:11:03 +0530

    [Deep]Added docStrings

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 class Stack:
+    """
+    : LIFO DS
+    """
     def __init__(self):
+        """
+        : Constructor init both stack
+        """
         self.min_stack = []
         self.max_stack = []
 
     def push(self, ele):
+        """
+        :param ele: num
+        :return: None
+        """
         pass
 
     def display(self):
+        """
+        : Display both the stacks
+        """
         pass
